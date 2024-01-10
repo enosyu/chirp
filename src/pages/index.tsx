94b78fa25ce962d8a97a1447edafbf8a023f19e2
enosyu <enos.yu@outlook.com>
2024-01-11 01:18:32 +0800

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.post.getAll.useQuery();
 
   return (
     <>
@@ -17,6 +17,10 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <UserButton afterSignOutUrl="/" />
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+
+        <div>
+          {data?.map((post) => <div key="post.id">{post.content}</div>)}
+        </div>
       </main>
     </>
   );
